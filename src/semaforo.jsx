b385226a71e873b6f369340893c4fb59888426e5
Ambrosio <ambrosio.rhc@gmail.com>
2024-03-04 21:37:07 -0300

    apagar agregado

--- src/semaforo.jsx
+++ src/semaforo.jsx
@@ -2,30 +2,40 @@ import React, { useState } from 'react';
 
 const Semaforo = () => {
 
-const [activeLight, setActiveLight] = useState (null);
+  const [activeLight, setActiveLight] = useState(null)
 
-const handleLight = (luz) => {setActiveLight(luz)
+  const handleLight = (luz) => setActiveLight(luz)
 
-}
+
 
   return (
     <>
       <div id="app">
+        <button
+          onClick={() => handleLight(null)}
+        >Apagame</button>
+        <button
+          onClick={() => handleLight("prendeTodo")}
+        >No apretar</button>
+
         <div id="container" >
-          <div id="trafficTop" className={activeLight === "" ? "" : null} 
-          onClick= {() => handleLight("")}
+          <div id="trafficTop"
+            onClick={() => handleLight(null)}
           ></div>
           <div
-            className={`luzRoja ${activeLight === "glowRed" ? "glowRed" : ""}`} 
+            className={`luzRoja ${activeLight === "glowRed" ? "glowRed" : null} 
+            ${activeLight === "prendeTodo" ? "prendeTodo" : null} `}
             onClick={() => handleLight("glowRed")}
           ></div>
           <div
-            className={`luzAmarilla ${activeLight === "glowYellow" ? "glowYellow" : ""} `}
-            onClick= {()=> handleLight("glowYellow")}
+            className={`luzAmarilla ${activeLight === "glowYellow" ? "glowYellow" : null}
+             ${activeLight === "prendeTodo" ? "prendeTodo" : null}`}
+            onClick={() => handleLight("glowYellow")}
           ></div>
           <div
-            className= {`luzVerde ${activeLight === "glowGreen" ? "glowGreen" : "" }`}
-            onClick= {() => handleLight("glowGreen")}
+            className={`luzVerde ${activeLight === "glowGreen" ? "glowGreen" : null} 
+             ${activeLight === "prendeTodo" ? "prendeTodo" : null}`}
+            onClick={() => handleLight("glowGreen")}
           ></div>
         </div>
       </div>
