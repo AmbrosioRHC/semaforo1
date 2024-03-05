b7a78bdc33c68d50527f97459bf52aaa0a8fe348
Ambrosio <ambrosio.rhc@gmail.com>
2024-03-04 21:08:35 -0300

    versión entregable

--- src/semaforo.jsx
+++ src/semaforo.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 
 const Semaforo = () => {
-  const [activeLight, setActiveLight] = useState(null);
 
-  const luzClickeada = (colorDeLuz) => {
-    setActiveLight(colorDeLuz);
-  };
+const [activeLight, setActiveLight] = useState (null);
+
+const handleLight = (luz) => {setActiveLight(luz)
+
+}
 
   return (
     <>
       <div id="app">
-        <div id="container">
-          <div id="trafficTop" onClick={() => luzClickeada(null)}></div>
-          <div
-            className={`luzRoja ${activeLight === 'red' ? 'glowRed' : ''}`}
-            onClick={() => luzClickeada('red')}
+        <div id="container" >
+          <div id="trafficTop" className={activeLight === "" ? "" : null} 
+          onClick= {() => handleLight("")}
           ></div>
           <div
-            className={`luzAmarilla ${activeLight === 'yellow' ? 'glowYellow' : ''}`}
-            onClick={() => luzClickeada('yellow')}
+            className={`luzRoja ${activeLight === "glowRed" ? "glowRed" : ""}`} 
+            onClick={() => handleLight("glowRed")}
           ></div>
           <div
-            className={`luzVerde ${activeLight === 'green' ? 'glowGreen' : ''}`}
-            onClick={() => luzClickeada('green')}
+            className={`luzAmarilla ${activeLight === "glowYellow" ? "glowYellow" : ""} `}
+            onClick= {()=> handleLight("glowYellow")}
+          ></div>
+          <div
+            className= {`luzVerde ${activeLight === "glowGreen" ? "glowGreen" : "" }`}
+            onClick= {() => handleLight("glowGreen")}
           ></div>
         </div>
       </div>
